2f4b357ea5a05747f7815a10f916a2b369fc072a
Yura Susuk <yurasusuk@gmail.com>
2014-06-16 16:51:01 +0200

    chat room actor was introduced

--- app/finance/ChatRoom.scala
+++ app/finance/ChatRoom.scala
@@ -1,14 +1,26 @@
 package finance
 
+import akka.actor.Actor
+
 /**
  * Created by iurii.susuk on 16.06.2014.
  */
-class ChatRoom {
+class ChatRoom extends Actor {
 
+  def receive = {
+    case Subscribe(user) =>
+    case Unsubscribe(user) =>
+    case UserPost(user, post) =>
+  }
 }
+
+// val chatRoom = new ChatRoom
+// chatRoom.start()
 
 case class User(name: String)
 case class Subscribe(user: User)
 case class Unsubscribe(user: User)
 case class Post(msg: String)
 case class UserPost(user: User, post: Post)
+
+
